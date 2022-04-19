dfa1c267c21823406942cffd49bd3355e9124761
diegoldiazr <diegoldiazr@gmail.com>
2022-04-19 09:31:06 +0200

    initial import
    initial import

---
+++ src/main/java/com/ddr/PostgreBootRestApplication.java
@@ -0,0 +1,13 @@
+package com.ddr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgreBootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgreBootRestApplication.class, args);
+	}
+
+}
